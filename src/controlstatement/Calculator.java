3b1c9604b58488bb1f57b41057a44ad6621e209b
[sushma133]git config --global user.name [sushma133] <reddy.sushma133@gmail.com>
2024-07-16 18:28:34 +0530

    3rd commit It contains the switch case statementproblems

---
+++ src/controlstatement/Calculator.java
@@ -0,0 +1,63 @@
+/*Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows : 
+	1 : + (Addition) a + b
+2 : - (Subtraction) a - b
+3 : * (Multiplication) a * b
+4 : / (Division) a / b
+5 : % (Modulo or remainder) a % b
+Calculate the result according to the operation given and display it to the user.
+*/
+package controlstatement;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	
+	public static void main(String []args)
+	{
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("My Calculator");
+		
+		System.out.println("Enter Your choice");
+		System.out.println("1.Addition");
+		System.out.println("2.Subtraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+		System.out.println("5.Modulo or remainder");
+		int opr=sc.nextInt();
+		System.out.println("Enter the First number:");
+		int a=sc.nextInt();
+		System.out.println("Enter the Second number:");
+		int b=sc.nextInt();
+		
+		
+		
+		switch(opr) {
+		case 1:
+			System.out.println("Sum:"+(a+b));
+	break;
+		case 2:
+			System.out.println("Subtraction:"+(a-b));
+			break;
+		case 3:
+			System.out.println("Multiplication:"+(a*b));
+			break;
+		case 4:
+			System.out.println("Divison:"+(a/b));
+			break;
+		case 5:
+			System.out.println("Modulo:"+(a%b));
+			break;
+			default:
+			System.out.println("Enter the valid option");
+		}
+		
+		
+		
+		sc.close();
+
+	}
+	
+}
